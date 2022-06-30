6af9acac077fdc89266fe4c39ac8d13970e08ac3
Luca Tomarelli <lucatomarelli1@gmail.com>
2022-06-30 15:22:39 +0200

    db fix

--- database/network.sql
+++ database/network.sql
@@ -1,6 +1,9 @@
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS resetCode;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS pending_requests;
+DROP TABLE IF EXISTS messages;
 
 CREATE TABLE users(
      id             SERIAL PRIMARY KEY,
@@ -20,19 +23,19 @@ CREATE TABLE resetCode(
      code           VARCHAR NOT NULL,
      email          VARCHAR NOT NULL,
      created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE pending_requests(
      id             SERIAL PRIMARY KEY,
      sender_id      int,
      recipient_id   int
-)
+);
 
 CREATE TABLE messages(
      id             SERIAL PRIMARY KEY,
      sender_id      int,
      text           VARCHAR
-)
+);
 
 CREATE TABLE events(
      id                  SERIAL PRIMARY KEY,
@@ -49,7 +52,7 @@ CREATE TABLE events(
      published              BOOLEAN,
      created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
-)
+);
 
-INSERT INTO events (creator, evt_name, startDate, startTime, endDate, endTime, evt_location, poster, evt_description, collaborators) 
+INSERT INTO events (creator, evt_name, start_date, start_time, end_date, end_time, evt_location, poster, evt_description, collaborators) 
 VALUES ('203','Autonoma','2022-01-21','20:00','2022-01-21','20:00','berlin','poster','description','collaborators');
