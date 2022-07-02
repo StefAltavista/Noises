588d3f6d289b072e0339afc802edc84c09283143
Stef Altavista <stefanomsc@hotmail.it>
2022-07-02 19:21:02 +0200

    cleanup server

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,10 @@
-/////DELETE OLD STUFF///////
+////////////SECURITY CHECK/////////////
+
+/////NEEDS PROPPER IMPLEMENTATION/////
+
+/////   ->  SEARCH BAR
+/////   ->  CREATE EVENT
+/////   ->  MESSAGES
 
 function checkRegistration(req, res, next) {
     const { email, first, last } = req.body;
@@ -21,69 +27,6 @@ function checkRegistration(req, res, next) {
     next();
 }
 
-function checkAdd(req, res, next) {
-    if (req.session.data) {
-        res.redirect("./sign");
-    }
-    next();
-    return;
-}
-
-function checkData(req, res, next) {
-    var { age, city, country, website } = req.body;
-    if (age == "" && city == "" && country == "" && website == "") {
-        res.redirect("/sign");
-        next();
-    }
-
-    if (!/^[0-9]+$/.test(age)) {
-        console.log("redirect", age);
-        age = null;
-        res.redirect("/profile");
-        next();
-    }
-    if (!website.startsWith("http://") || !website.startsWith("https://")) {
-        website = "http://" + website;
-    }
-
-    return { age, city, country, website };
-}
-function checkSignature(req, res, next) {
-    if (req.route.path == "/sign" && req.session.signature) {
-        res.redirect("./thanks");
-    } else if (req.route.path != "/sign" && !req.session.signature) {
-        res.redirect("./sign");
-        next();
-    } else {
-        console.log("in sig");
-        next();
-        return;
-    }
-}
-
-//not a middleware
-function checkQuery(req) {
-    if (req.query) {
-        var { ct, cn } = req.query;
-        var verify;
-
-        if (ct) {
-            verify = /^[a-zA-Z]+$/.test(ct);
-            verify ? "" : (ct = null);
-        }
-        if (cn) {
-            verify = /^[a-zA-Z]+$/.test(cn);
-            verify ? "" : (cn = null);
-        }
-
-        return { ct, cn };
-    } else return { ct: null, cn: null };
-}
 module.exports = {
     checkRegistration,
-    checkAdd,
-    checkData,
-
-    checkSignature,
-    checkQuery,
 };
